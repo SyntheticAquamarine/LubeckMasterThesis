06337e8585542aaa38bbabf59c0f9c612479edd8
Marek Jędrychowski <janjasielski.j@gmail.com>
2023-04-03 16:08:17 +0200

    Created new data shuffling, feature selection as a separate file

---
+++ DataShuffling.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+def data_shuffler(merged_dir, shuffled_data):
+    
+    X_to_shuffle = f'{merged_dir}X.npy'
+    y_to_shuffle = f'{merged_dir}y.npy'
+    
+    
+    dataset = np.load(X_to_shuffle)
+    labels = np.load(y_to_shuffle)
+    shuffler = np.random.permutation(len(dataset))
+    X = dataset[shuffler]
+    y = labels[shuffler]
+    
+    np.save(f'{shuffled_data}/X_shuffled', X)
+    np.save(f'{shuffled_data}/y_shuffled', y)
+
+    return X,y
