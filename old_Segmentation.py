abf13c818bbe44535ce2100265359365274cff85
SyntheticAquamarine <janjasielski.j@gmail.com>
2023-05-05 11:18:30 +0200

    Additional files merged in segmentation

--- old_Segmentation.py
+++ old_Segmentation.py
@@ -1,5 +1,4 @@
 import os
-import pandas as pd
 from pandas import read_csv
 import re
 from os import walk
@@ -28,7 +27,7 @@ def file_import(MainPath):
                     NewName = re.sub('._CsvLog', 'CsvLog', name)
                     filenameList.append(NewName)
                     fullPath.append(Path + '/' + NewName)
-                    
+                   
                    
                    
 def segmentation(pd_data, window_size):
@@ -51,85 +50,44 @@ def segmentation(pd_data, window_size):
 
 
 
-def file_processing(T, WindowSize, MainPath):
+def file_processing(T, WindowSize, NumOfSensors, MainPath):
     file_import(MainPath)
     #T = 'N' #500000 #'N' # describes which rows multiplied by n should be taken into the dataset #If chosen parameter is N the rows will not get dropped
-    fields = ['Time', 'Infinity|RESP.ONLY_ONE_IN_GROUP [OHM]', 'Infinity|SPO2.SPO2_PULSE [COUNTS]']
+    fields = ['Infinity|RESP.ONLY_ONE_IN_GROUP [OHM]', 'Infinity|SPO2.SPO2_PULSE [COUNTS]']
     #WindowSize = 100
     #NumOfSensors = 2
+    df_0 = np.zeros((1,WindowSize, NumOfSensors))
+    df_1 = np.zeros((1,WindowSize, NumOfSensors))
 
     files_total = len(fullPath)
     i = 1
-    
-    
-    SensorPath = 'C:/Users/janja/LubeckMasterThesis/SensorFiles'
-
-    directory = os.fsencode(SensorPath)
-     
- 
     for path in fullPath:
-        print(path)
-        
+        #print(path)
         df_local = read_csv(path, sep = ',', encoding = 'UTF-8', usecols=fields)
-        df_local.rename(columns = {"Time":"timestamp"}, inplace = True)
         
-        
-        imported = re.sub('\/','',re.search('\/\d{1,2}\/[B,R]\/',path).group())
-        print(f'The filename is: {imported}')
-        
-        sensor = read_csv(f'C:/Users/janja/LubeckMasterThesis/SensorFiles/{imported}.csv', sep = ';', header = 0)
-        
-        sensor = sensor[sensor.columns[1:]]
-        sensor['timestamp'] = sensor['timestamp'].str.slice(start=11)
-        sensor['timestamp'] = sensor['timestamp'].str.strip()
+        print(df_local.shape)
 
         df_local = df_local.interpolate()
-        df_local.dropna(thresh = 3, inplace = True)
-    
+        print(df_local.shape)
+        df_local.dropna(thresh = 2, inplace = True)
+        print(df_local.shape)
         
-        df_local['timestamp'] = pd.to_datetime(df_local['timestamp'], format='%H:%M:%S.%f')
-        df_local = df_local.set_index('timestamp')
-        sensor['timestamp'] = pd.to_datetime(sensor['timestamp'], format='%H:%M:%S.%f')
-        sensor = sensor.set_index('timestamp')
-        print(f'Base frequency {sensor.index.freq}')
-        sensor = sensor.resample('10ms').mean()
-        print(f'Upsampled frequency {sensor.index.freq}')
-        sensor = sensor.interpolate()
-
         
-        print(f'sensor shape: {sensor.shape}')
-        print(f' df_local shape {df_local.shape}')
-        df_local = df_local.sort_values('timestamp')
-        sensor = sensor.sort_values('timestamp')
-        df_joined = pd.merge_asof(df_local, sensor, on = 'timestamp' , tolerance = pd.Timedelta('50ms'))
-        df_joined = df_joined.drop(columns = ['timestamp'], axis = 1)
-    
-        
-        print(f'na number pre dropna {df_joined.isna().sum()}')
-        print(f'df joined shape {df_joined.shape}')
-        df_joined.dropna(thresh = 8, inplace = True)
-        print(f'na number post dropna {df_joined.isna().sum()}')
-        print(f'df joined shape {df_joined.shape}')    
-        print(df_joined)
-
-    
-    #print(f'Number of blank spaces for the position: \n {df_local.isna().sum()}')
+        print(path)
+        print(f'Number of blank spaces for the position: \n {df_local.isna().sum()}')
         if T != 'N':
-            df_joined = df_joined[df_joined.index % T == 0] #Set to 2000 as 1 second is 20 observations
+            df_local = df_local[df_local.index % T == 0] #Set to 2000 as 1 second is 20 observations
         match = re.findall("\/B\/Csv",path)
         
-    
-        
-        df_segmented = segmentation(df_joined, WindowSize)
+        df_segmented = segmentation(df_local, WindowSize)
         
         print('df_segmented shape')
         print(df_segmented.shape)
         print(sum(sum(np.isnan(df_segmented))))
         
-                
+              
         neo = re.findall('\/([\d]{1,2})\/', path)
 
-        
         if bool(match) == True:
             location_B = f'./CreatedFiles/Segmentation/{neo[0]}_B.npy'
             np.save(location_B, df_segmented)
